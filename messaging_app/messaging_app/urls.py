5cdc1e85f0b4f8ca51993c4d954688239a8311fb
WilliamNyamu <williamnyamu08@gmail.com>
2025-11-21 16:54:55 +0300

    Refactored messaging urls.py

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+api-auth = None
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('chats.urls')),
