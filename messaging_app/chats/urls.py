5f98b9a1c88fd079364f8cb527812d688a3dfd63
WilliamNyamu <williamnyamu08@gmail.com>
2025-11-21 16:51:23 +0300

    Improved the url structure

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,7 +1,9 @@
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import NestedDefaultRouter, DefaultRouter
 from .views import UserViewSet, ConversationViewSet, MessageViewSet
+from django.urls import path, include
 
 router = DefaultRouter()
+router = NestedDefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'conversations', ConversationViewSet)
 router.register(r'messages', MessageViewSet)
