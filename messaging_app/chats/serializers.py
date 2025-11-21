18b7720f764c85d4c4575371d2c0ca849514077f
WilliamNyamu <williamnyamu08@gmail.com>
2025-11-21 16:11:01 +0300

    Refactored serializers.py

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -6,12 +6,18 @@ User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        pass
+        model = User
+        fields = ['user_id', 'email', 'first_name', 'last_name', 'phone_number', 'role']
 
 class MessageSerializer(serializers.ModelSerializer):
+    sender_id = UserSerializer(read_only=True)
     class Meta:
-        pass
+        model = Message
+        fields = ['id', 'message_id', 'sender_id', 'sent_at']
+        read_only_fields = ['sent_at']
+    
 
 class ConversationSerializer(serializers.ModelSerializer):
     class Meta:
-        pass
+        model = Conversation
+        fields = ['conversation_id', 'participants_id', 'created_at']
