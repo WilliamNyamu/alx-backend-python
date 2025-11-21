a9bbd60525528f27a03cf5013ce10708d15f93fe
WilliamNyamu <williamnyamu08@gmail.com>
2025-11-21 16:45:39 +0300

    Refactored urls.py

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,4 +1,3 @@
-from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, ConversationViewSet, MessageViewSet
 
@@ -7,6 +6,4 @@ router.register(r'users', UserViewSet)
 router.register(r'conversations', ConversationViewSet)
 router.register(r'messages', MessageViewSet)
 
-urlpatterns = [
-    path('', include(router.urls)),
-]
+urlpatterns = router.urls
