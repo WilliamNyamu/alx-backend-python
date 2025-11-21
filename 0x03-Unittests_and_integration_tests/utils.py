508c04a4b567d4c3a865b0af435fcfc0db154020
WilliamNyamu <williamnyamu08@gmail.com>
2025-11-21 09:26:49 +0300

    Removed the trailing 1

--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -73,6 +73,3 @@ def memoize(fn: Callable) -> Callable:
         return getattr(self, attr_name)
 
     return property(memoized)
-
-nested_map = {"a": {"b": {"c": 1}}}
-print(access_nested_map(nested_map, ["a", "b", "c"]))
