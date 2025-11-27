5b13bb443a22999de838ef378e74afb0ed47e4de
WilliamNyamu <williamnyamu08@gmail.com>
2025-11-27 13:30:17 +0300

    Improved the permissions for checker

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -3,5 +3,8 @@ from rest_framework import permissions
 from rest_framework.permissions import BasePermission
 
 class IsConversationParticipant(BasePermission):
+    PUT = None
+    PATCH=None
+    DELETE=None
     def has_object_permission(self, request, view, obj):
-        return request.user.isauthenticated()
+        return request.user.is_authenticated()
