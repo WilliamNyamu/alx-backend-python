fc0d12dfc2a399883cdb28283e4032aceefd6b29
WilliamNyamu <williamnyamu08@gmail.com>
2025-11-27 18:18:13 +0300

    Implemented a middleware for logging

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,1 +1,23 @@
+# Create a middleware that logs each user’s requests to a file, including the 
+# timestamp, user and the request path.
+
 from django.conf import settings
+from django.core.exceptions import PermissionDenied, MiddlewareNotUsed
+from datetime import datetime
+import logging
+
+class RequestLoggingMiddleware:
+    """
+    Middleware that logs each user's requests to a file, including the timestamp, user, and path
+    """
+    def __init__(self, get_response):
+        self.get_response = get_response
+    
+    def __call__(self, request):
+
+        logging.basicConfig(filename='request.log', filemode='a', encoding='UTF8')
+        logging.info(f"{datetime.now()} - User:{request.user} - Path:{request.path}")
+
+        response = self.get_response(request)
+
+        return response
