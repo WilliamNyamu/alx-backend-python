0bbdb00639fc3647cf37efbb1f63de022ae482a0
WilliamNyamu <williamnyamu08@gmail.com>
2025-11-29 21:49:33 +0300

    Implemented a custom middleware for accesstime for chats

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -3,7 +3,7 @@
 
 from django.conf import settings
 from django.core.exceptions import PermissionDenied, MiddlewareNotUsed
-from datetime import datetime
+from datetime import datetime, time
 import logging
 
 class RequestLoggingMiddleware:
@@ -38,4 +38,24 @@ class RequestLoggingMiddleware:
 
         self.logger.info(f"{datetime.now()} - User: {user} - Path: {request.path}")
 
+        return response
+
+
+class RequestAccessByTimeMiddleware:
+    """
+    Checks server time and denies access if time is outside 9pm and 6pm
+    """
+    def __init__(self, get_response):
+        self.get_response = get_response
+    
+    def __call__(self, request):
+        
+        now = datetime.now().time()
+        six = time(18, 0) # Notice that this time is gotten from: from datetime import datetime, time
+        nine = time(21, 0)
+
+        if not (six <= now <= nine):
+            raise PermissionDenied()
+
+        response = self.get_response(request)
         return response
--- Django-Middleware-0x03/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -62,6 +62,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'chats.middleware.RequestLoggingMiddleware', # Custom Middleware for logging
+    'chats.middleware.RequestAccessByTimeMiddleware',
 ]
 
 ROOT_URLCONF = 'messaging_app.urls'
